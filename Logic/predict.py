f4a0f92e07901105ddb95cfb9931d5b67d257175
Daisy <daisyap2005@gmail.com>
2026-04-14 18:35:32 -0400

    Add files via upload

--- Logic/predict.py
+++ Logic/predict.py
@@ -4,21 +4,32 @@ import torch
 from torchvision import transforms
 from PIL import Image
 from Models.CNN_Model import BrainCNN
+from Models.CNN_Model2 import NN
+from torch.functional import F
 
-def predict_image(image_path):
+def predict_image(image_path, model):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load model
-    model = BrainCNN()
-    model.load_state_dict(torch.load("saved_models/brain_cnn.pth", map_location=device))
+    if model == 1:
+        model = BrainCNN()
+        model.load_state_dict(torch.load("saved_models/brain_cnn.pth", map_location=device))
+        transform = transforms.Compose([
+            transforms.Resize((128, 128)),
+            transforms.ToTensor()
+        ])
+    elif model == 2:
+        model = NN()
+        model.load_state_dict(torch.load("saved_models/CNN_Model2.pth", map_location=device))
+        transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
+        ])
+
     model.to(device)
     model.eval()
 
     # Image transform (same as training)
-    transform = transforms.Compose([
-        transforms.Resize((128, 128)),
-        transforms.ToTensor()
-    ])
 
     # Load image
     image = Image.open(image_path).convert("RGB")
@@ -29,8 +40,9 @@ def predict_image(image_path):
     # Predict
     with torch.no_grad():
         outputs = model(image)
-        _, predicted = torch.max(outputs, 1)
+        probabilities = F.softmax(outputs, dim=1)
+        confidence, predicted = torch.max(probabilities, 1)
 
     classes = ["brain_tumor", "healthy"]
 
-    return classes[predicted.item()]
+    return classes[predicted.item()], confidence.item()
--- Logic/test.py
+++ Logic/test.py
@@ -1,7 +1,7 @@
 import torch
 
 def test_model(model, test_loader):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model.to(device)
     model.eval()
 
@@ -11,11 +11,10 @@ def test_model(model, test_loader):
     with torch.no_grad():
         for images, labels in test_loader:
             images, labels = images.to(device), labels.to(device)
-
             outputs = model(images)
             _, predicted = torch.max(outputs, 1)
 
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print("Test Accuracy:", 100 * correct / total)
+    print(f"{model.__class__.__name__} Test Accuracy: ", 100 * correct / total)
