3f94b00c0ce7e1d376e6ce8cb0f0e2c3a3693c69
Daisy <daisyap2005@gmail.com>
2026-04-16 17:48:06 -0400

    Add files via upload

--- Logic/test.py
+++ Logic/test.py
@@ -1,20 +1,26 @@
 import torch
+from torchmetrics.classification import BinaryAccuracy, BinaryPrecision, BinaryRecall, BinaryF1Score
 
 def test_model(model, test_loader):
+    accuracy = BinaryAccuracy()
+    precision = BinaryPrecision()
+    recall = BinaryRecall()
+    f1 = BinaryF1Score()
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model.to(device)
     model.eval()
 
-    correct = 0
-    total = 0
-
     with torch.no_grad():
         for images, labels in test_loader:
             images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
+            if model.__class__.__name__ != "Ensemble":
+                outputs = model(images)
+            else:
+                outputs = model(images, images)
             _, predicted = torch.max(outputs, 1)
 
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    print(f"{model.__class__.__name__} Test Accuracy: ", 100 * correct / total)
+    print(f"{model.__class__.__name__} Test Accuracy: ", accuracy(predicted, labels).item() * 100, "%")
+    print(f"{model.__class__.__name__} Test Precision: ", precision(predicted, labels).item() * 100, "%")
+    print(f"{model.__class__.__name__} Test Recall: ", recall(predicted, labels).item() * 100, "%")
+    print(f"{model.__class__.__name__} Test F1 Score: ", f1(predicted, labels).item() * 100, "%")
