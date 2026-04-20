e36045c4d931ad6a2d3726bae93ec3477a2c2ea3
Daisy <daisyap2005@gmail.com>
2026-04-20 12:51:53 -0400

    Add files via upload

--- Models/EnsembleCNN.py
+++ Models/EnsembleCNN.py
@@ -14,27 +14,34 @@ class Ensemble(nn.Module):
         super(Ensemble, self).__init__()
         self.CNN1 = CNN1
         self.CNN2 = CNN2
-        self.classifier = nn.Linear(43264,12544)
+        self.classifier = nn.Linear(4,2)
 
     def forward(self, x1, x2):
-        x1 = self.CNN1(x1)
-        x2 = self.CNN2(x2)
-        x = torch.cat((x1, x2), 1)
-        print(x)
-        x = self.classifier(F.relu(x))
+        x1 = F.softmax(self.CNN1(x1))
+        x2 = F.softmax(self.CNN2(x2))
+        x = (x1 * 0.7 + x2 * .3)/2 # Fixed proper concatenation; also add weight to better performing model
+        #print(x)
+        #x = self.classifier(F.relu(x))
         return x
 
 # Testing
 TEST_PATH = "/Users/daisyaptovska/Desktop/CMPSC441_FinalProject/Data/test/"
 
-transform = transforms.Compose([
+transform1 = transforms.Compose([
+            transforms.PILToTensor(),
+            transforms.ConvertImageDtype(torch.float),
+            transforms.Resize((128, 128))
+        ])
+transform2 = transforms.Compose([
             transforms.PILToTensor(),
             transforms.ConvertImageDtype(torch.float),
             transforms.Resize((224, 224))
         ])
 
-test_data = torchvision.datasets.ImageFolder(root=TEST_PATH, transform=transform)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True)
+test_data1 = torchvision.datasets.ImageFolder(root=TEST_PATH, transform=transform1)
+test_loader1 = torch.utils.data.DataLoader(test_data1, batch_size=64, shuffle=True)
+test_data2 = torchvision.datasets.ImageFolder(root=TEST_PATH, transform=transform2)
+test_loader2 = torch.utils.data.DataLoader(test_data2, batch_size=64, shuffle=True)
 
 CNN1 = BrainCNN()
 CNN2 = NN()
@@ -43,5 +50,5 @@ CNN1.load_state_dict(torch.load("/Users/daisyaptovska/Desktop/CMPSC441_FinalProj
 CNN2.load_state_dict(torch.load("/Users/daisyaptovska/Desktop/CMPSC441_FinalProject/Saved_Models/CNN_Model2.pth"))
 
 model = Ensemble(CNN1, CNN2)
-test_model(model, test_loader)
+test_model(model, test_loader1, test_loader2)
 
