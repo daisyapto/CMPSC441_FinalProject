87542289f223f06701f3caf38d46e087e18959c5
Daisy <daisyap2005@gmail.com>
2026-04-21 18:36:05 -0400

    Add files via upload

--- Manual_Test.py
+++ Manual_Test.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-def manual_test(model):
+def manual_test(model_num):
     root = tk.Tk()
     root.withdraw()
 
@@ -13,6 +13,6 @@ def manual_test(model):
     )
 
     if file_path:
-        result, confidence = predict_image(file_path, model)
+        confidence, result = predict_image(file_path, model_num)
         print("Prediction:", result)
         print("Confidence:", confidence * 100, "%")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Models.CNN_Model import BrainCNN
 from Models.CNN_Model2 import NN
+from Models.EnsembleCNN import Ensemble
 from Logic.test import test_model
 from Logic.predict import predict_image
 from Manual_Test import manual_test
@@ -38,13 +39,16 @@ def main():
     CNN2.load_state_dict(state2)
     CNN2.eval()
 
+    ensemble = Ensemble(CNN1, CNN2)
+
     test_model(CNN1, test_loader1)
     test_model(CNN2, test_loader2)
+    test_model(ensemble, test_loader1, test_loader2)
 
     more = int(input("\n0) Exit\n1) Enter an image to classify\n>>>"))
     while more == 1:
-        model = int(input("\n1) CNN Model 1\n2) CNN Model 2\n3) Ensemble (CNN Model 1 + CNN Model 2)\n>>>"))
-        manual_test(model)
+        model_num = int(input("\n1) CNN Model 1\n2) CNN Model 2\n3) Ensemble (CNN Model 1 + CNN Model 2)\n>>>"))
+        manual_test(model_num)
         more = int(input("\n0) Exit\n1) Enter another image to classify\n>>>"))
 
 if __name__ == '__main__':
